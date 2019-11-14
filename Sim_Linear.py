10983f71bb4959bc479d4f6f0ebacdd914bb82ae
Jay <jayaramakrishnan.jrk@gmail.com>
2019-11-14 12:14:05 +0000

    Load CSV and Describe data

--- Sim_Linear.py
+++ Sim_Linear.py
@@ -1,5 +1,14 @@
+
 # Import packages
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Load the dataset and Describe the data
+
+data = pd.read_csv("tvmarketing.csv")
+data.head()
+
+data.info()
+data.describe()
